d6149ad77b6776d304774220c8d6f8ecf2d1c76e
changhyen <kuku0746@naver.com>
2025-08-24 23:06:36 +0900

    2번째 과제 완료!

---
+++ Happy_Java/src/src/For_hw.java
@@ -0,0 +1,39 @@
+package src;
+
+public class For_hw {
+    public static void main(String[] args) {
+//        ### 2단계: 반복문
+//
+//        3. 1부터 100까지의 합을 구하는 프로그램을 작성하세요.
+//        4. 구구단을 출력하는 프로그램을 작성하세요. (2단\~9단)
+//        5. 1부터 50까지 수 중에서 **3의 배수이면서 5의 배수인 수**만 출력하세요.
+//
+//        int sum =0;
+//        for (int i = 1; i <= 100; i++) { //1~100까지의 합
+//            sum +=i;
+//        }
+//        System.out.println(sum);
+//
+//-----------------------------------------------------------
+//
+//        for (int i = 1; i <= 9; i++) { //구구단 출력
+//            System.out.println(i + "단\t");
+//            for (int j = 1; j <= 9; j++) {
+//                System.out.print(i * j + " ");
+//            }
+//            System.out.println();
+//            System.out.println();
+//        }
+//
+//-------------------------------------------------------------
+//
+//        for (int i = 1; i < 50; i++) {
+//            if (i%3==0 && i%5==0){
+//                System.out.println(i);
+//            }
+//        }
+
+
+    }
+}
+
